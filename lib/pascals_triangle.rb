815380e37d6b64fa2c37dcce19f3fc69b5625466
Bojan Babic <bojance93@gmail.com>
2017-10-05 19:54:08 +0200

    Add triangle project

---
+++ lib/pascals_triangle.rb
@@ -0,0 +1,37 @@
+class Triangle
+
+  def initialize rows
+    @rows = rows
+  end
+
+  def print_triangle
+    if @rows == 0
+      return [1]
+    elsif @rows == 1
+      return [[1], [1, 1]]
+    else
+      result = [[1], [1, 1]]
+
+      previous_row = [1, 1]
+      n=3
+      while n <= @rows
+        next_row = [1]
+
+        i = 0
+        while i < previous_row.length - 1
+          sum = previous_row[i] + previous_row[i+1]
+          next_row << sum
+          i+=1
+        end
+
+        next_row << 1
+        result << next_row
+        previous_row = next_row
+        n+=1
+      end
+
+    end
+    result
+  end
+
+end
